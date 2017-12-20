7027fef2e321db0c6939d9b6ac2545ad21471a79
crown <crown-huang@foxmail.com>
2017-12-20 23:12:32 +0800

    add read_pems_occupancy_speed.py

---
+++ read_pems_occupancy_speed.py
@@ -0,0 +1,67 @@
+import numpy as np
+import csv
+
+no = [500011092, 500011102, 500011112, 500011113, 500014111, 500014112]
+one_day_occupancy = np.zeros((288, 147))
+one_day_speed = np.zeros((288, 147))
+csv_reader = csv.reader(open('../Trafficdata/2013pemsd05RawData/d05_text_station_5min_2013_01_01.txt'))
+i = 0  # i是one_year_occupancy的行数
+j = 0  # j是one_year_occupancy的列数
+for row in csv_reader:
+    if int(row[1]) in no:
+        continue
+    try:  # 数据中有缺失暂时是用-1来代替
+        one_day_occupancy[i][j] = float(row[10])
+    except Exception as e:
+        one_day_occupancy[i][j] = -1.0
+    try:
+        one_day_speed[i][j] = float(row[11])
+    except Exception as e:
+        one_day_speed[i][j] = -1.0
+    j += 1
+    if j == 147:
+        i += 1
+        j = 0
+occupancy = np.array(one_day_occupancy)
+speed = np.array(one_day_speed)
+
+for m in range(1, 13):
+    for d in range(1, 32):
+        if m == 1 and d == 1 or m == 3 and d == 10:
+            continue
+        if m < 10:
+            m_str = '0' + str(m)
+        else:
+            m_str = str(m)
+        if d < 10:
+            d_str = '0' + str(d)
+        else:
+            d_str = str(d)
+        try:  # 为了避免程序读到像2月30日这样的月份而停止
+            csv_reader = csv.reader(
+                open('../Trafficdata/2013pemsd05RawData/d05_text_station_5min_2013_%s_%s.txt' % (m_str, d_str)))
+        except Exception as e:
+            print('month ' + m_str + ' day ' + d_str + ' does not exist!')
+            continue
+        i = 0  # i是one_year_occupancy的行数
+        j = 0  # j是one_year_occupancy的列数
+        for row in csv_reader:
+            if int(row[1]) in no:
+                continue
+            try:
+                one_day_occupancy[i][j] = float(row[10])  # 从0开始
+            except Exception as e:
+                one_day_occupancy[i][j] = -1.0
+            try:
+                one_day_speed[i][j] = float(row[11])
+            except Exception as e:
+                one_day_speed[i][j] = -1.0
+            j += 1
+            if j == 147:
+                i += 1
+                j = 0
+        occupancy = np.vstack((occupancy, one_day_occupancy))
+        speed = np.vstack((speed, one_day_speed))
+        print('month' + m_str + ' day ' + d_str + ' is finished!')
+
+np.savez('data/pems_speed_occupancy.npz', speed=speed, occupancy=occupancy)
