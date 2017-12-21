c6f1e68579c757de05a377a8016ef587b4394d68
crown <crown-huang@foxmail.com>
2017-12-21 20:59:02 +0800

    add abnormal_data_process and normalize to getdata.py

--- get_data.py
+++ get_data.py
@@ -2,9 +2,15 @@ import scipy.io as sio
 import numpy as np
 
 
-def get_pems_data(step=1, train_num=71 * 96, test_num=18 * 96):
+def train_fso_test_f(step=1, train_num=71 * 96, test_num=18 * 96):
     load_data = sio.loadmat('data/new147k1.mat')
-    data = load_data['data']
+    pems = np.load('data/pems_speed_occupancy_15min.npz')
+    flow = abnormal_data_process(load_data['data'])
+    speed = abnormal_data_process(pems['speed'])
+    occupancy = abnormal_data_process(pems['occupancy'])
+    speed = normalize(speed)
+    occupancy = normalize(occupancy)
+    data = np.hstack((flow, speed, occupancy))
 
     train_x = []
     train_y = []
@@ -13,11 +19,11 @@ def get_pems_data(step=1, train_num=71 * 96, test_num=18 * 96):
 
     for i in range(train_num):
         train_x.append(data[i:i + step, :])
-        train_y.append(data[i + step, :])
+        train_y.append(flow[i + step, :])
 
     for i in range(test_num):
         test_x.append(data[train_num + i:train_num + i + step, :])
-        test_y.append(data[train_num + i + step, :])
+        test_y.append(flow[train_num + i + step, :])
 
     train_x_np = np.array(train_x)
     train_y_np = np.array(train_y)
@@ -30,3 +36,15 @@ def get_pems_data(step=1, train_num=71 * 96, test_num=18 * 96):
     test_y_np[test_y_np <= 0] = 3 / 1956
 
     return train_x_np, train_y_np, test_x_np, test_y_np
+
+
+def abnormal_data_process(data):
+    # 从0开始的24,25,26,27需要去掉
+    data = np.delete(data, [24, 25, 26, 27], axis=1)
+    return data
+
+
+def normalize(data):
+    dmin, dmax = data.min(), data.max()  # 求最大最小值
+    data = (data - dmin) / (dmax - dmin)
+    return data
