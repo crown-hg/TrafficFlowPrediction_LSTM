45a2390bbea121dde7aed584033bad1ac80c9a93
crown <crown-huang@foxmail.com>
2017-12-24 15:22:58 +0800

    add fso->f and f->f and abnormal process and normalize

--- get_data.py
+++ get_data.py
@@ -38,6 +38,36 @@ def train_fso_test_f(step=1, train_num=71 * 96, test_num=18 * 96):
     return train_x_np, train_y_np, test_x_np, test_y_np
 
 
+def train_f_test_f(step=1, train_num=71 * 96, test_num=18 * 96):
+    load_data = sio.loadmat('data/new147k1.mat')
+    flow = abnormal_data_process(load_data['data'])
+    data = flow
+    train_x = []
+    train_y = []
+    test_x = []
+    test_y = []
+
+    for i in range(train_num):
+        train_x.append(data[i:i + step, :])
+        train_y.append(flow[i + step, :])
+
+    for i in range(test_num):
+        test_x.append(data[train_num + i:train_num + i + step, :])
+        test_y.append(flow[train_num + i + step, :])
+
+    train_x_np = np.array(train_x)
+    train_y_np = np.array(train_y)
+    test_x_np = np.array(test_x)
+    test_y_np = np.array(test_y)
+
+    train_x_np[train_x_np <= 0] = 3 / 1956
+    train_y_np[train_y_np <= 0] = 3 / 1956
+    test_x_np[test_x_np <= 0] = 3 / 1956
+    test_y_np[test_y_np <= 0] = 3 / 1956
+
+    return train_x_np, train_y_np, test_x_np, test_y_np
+
+
 def abnormal_data_process(data):
     # 从0开始的24,25,26,27需要去掉
     data = np.delete(data, [24, 25, 26, 27], axis=1)
