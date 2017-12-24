fedb8d533730150bfed25338d08327677b94019c
crown <crown-huang@foxmail.com>
2017-12-24 23:17:47 +0800

    make lstm functional

--- lstm_multicell.py
+++ lstm_multicell.py
@@ -2,73 +2,61 @@
 import time
 import datetime
 import tensorflow as tf
+import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.contrib import rnn
 import get_data as gd
 
 
-def lstm_test(hidden_size, layer_num, dropout_rate):
-    start_time = datetime.datetime.now()
-
-    # 设置 GPU 按需增长
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-
-    ####################################
-    #  设置超参数
-
-    lr = 1e-3
-    # 在训练和测试的时候，我们想用不同的 batch_size.所以采用占位符的方式
-    batch_size = tf.placeholder(tf.int32, [])  # 注意类型必须为 tf.int32
-    # 在 1.0 版本以后请使用 ：
-    # keep_prob = tf.placeholder(tf.float32, [])
-    # batch_size = tf.placeholder(tf.int32, [])
-
-    # 每个时刻的输入特征是28维的，就是每个时刻输入一行，一行有 28 个像素
-    input_size = 143
-    # 所用时间段的个数
-    timestep_size = 4
-    # 每个隐含层的节点数
-    # hidden_size = 200
-    # LSTM layer 的层数
-    # layer_num = 2
-    # dropout
+def lstm_test(hidden_size, layer_num, max_epoch, dropout_keep_rate, train_x, train_y, test_x, test_y, file_name):
+    # 所用时间段的个数 timestep_size = 4
+    # 每个隐含层的节点数hidden_size = 200
+    # LSTM layer 的层数layer_num = 2
     # dropout_rate = 0.5
-    # 输出的结点数
-    output_size = 143
-    # 训练次数
-    max_epoch = 20000
-    # 训练集大小
-    train_num = 71 * 96
-    # 测试集大小
-    test_num = 18 * 96
-    sess = tf.Session(config=config)
-    _X = tf.placeholder(tf.float32, [None, timestep_size, input_size])
-    _Y = tf.placeholder(tf.float32, [None, output_size])
-    # dropout的留下的神经元的比例
-    keep_prob = tf.placeholder(tf.float32, [])
+    # 输出的结点数output_size = 143
+    # 训练次数max_epoch = 20000
 
-    ####################################################################
-    # 根据timestep_size获取数据
-    train_x, train_y, test_x, test_y = gd.train_f_test_f(timestep_size)
+    config = tf.ConfigProto(log_device_placement=True)
+    # 设置 GPU 按需增长
+    # config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    ####################################
+
+    # train_num训练集大小,input_size输入维度
+    train_num, time_step_size, input_size = train_x.shape
+    # output_size输出维度
+    _, output_size = train_y.shape
+    # train_num测试集大小
+    test_num, _, _ = test_x.shape
 
     # 下面几个步骤是实现 RNN / LSTM 的关键
     ####################################################################
-    # **步骤1：RNN 的输入shape = (batch_size, timestep_size, input_size)
-    X = _X
+    # **步骤1：LSTM 的输入shape = (batch_size, timestep_size, input_size)，输出shape=(batch_size, output_size)
+    _X = tf.placeholder(tf.float32, [None, time_step_size, input_size])
+    _Y = tf.placeholder(tf.float32, [None, output_size])
 
-    def lstm_multi_cell(cell_num):
-        # **步骤2：定义LSTM_cell，只需要说明 hidden_size, 它会自动匹配输入的 X 的维度
-        lstm_multi_cell = []
+    # dropout的留下的神经元的比例
+    keep_prob = tf.placeholder(tf.float32, [])
+
+    # 在训练和测试的时候，我们想用不同的 batch_size.所以采用占位符的方式
+    batch_size = tf.placeholder(tf.int32, [])  # 注意类型必须为 tf.int32
+
+    # 学习率
+    lr = 1e-3
+
+    def multi_cells(cell_num):
+        # 多cell的lstm必须多次建立cell保存在一个list当中
+        multi_cell = []
         for _ in range(cell_num):
+            # **步骤2：定义LSTM_cell，只需要说明 hidden_size, 它会自动匹配输入的 X 的维度
             lstm_cell = rnn.BasicLSTMCell(num_units=hidden_size, forget_bias=1.0, state_is_tuple=True)
             # **步骤3：添加 dropout layer, 一般只设置 output_keep_prob
             lstm_cell = rnn.DropoutWrapper(cell=lstm_cell, input_keep_prob=1.0, output_keep_prob=keep_prob)
-            lstm_multi_cell.append(lstm_cell)
-        return lstm_multi_cell
+            multi_cell.append(lstm_cell)
+        return multi_cell
 
     # **步骤4：调用 MultiRNNCell 来实现多层 LSTM
-    mlstm_cell = rnn.MultiRNNCell(lstm_multi_cell(layer_num), state_is_tuple=True)
+    mlstm_cell = rnn.MultiRNNCell(multi_cells(layer_num), state_is_tuple=True)
 
     # **步骤5：用全零来初始化state
     init_state = mlstm_cell.zero_state(batch_size, dtype=tf.float32)
@@ -95,7 +83,7 @@ def lstm_test(hidden_size, layer_num, dropout_rate):
     #         (cell_output, state) = mlstm_cell(X[:, timestep, :], state)
     #         outputs.append(cell_output)
     # h_state = outputs[-1]
-    outputs, state = tf.nn.dynamic_rnn(mlstm_cell, inputs=X, initial_state=init_state, time_major=False)
+    outputs, state = tf.nn.dynamic_rnn(mlstm_cell, inputs=_X, initial_state=init_state, time_major=False)
     h_state = outputs[:, -1, :]  # 或者 h_state = state[-1][1]
 
     # 输出层
@@ -123,38 +111,36 @@ def lstm_test(hidden_size, layer_num, dropout_rate):
     train_mre_reult = []
     test_mre_reult = []
     # 训练和测试
+    start_time = datetime.datetime.now()
     for i in range(1, max_epoch + 1):
-        if i % 50 == 0:
+        if i % 5 == 0:
             train_mre, train_mae, train_rmse = model_run(train_x, train_y, 1.0, train_num)
             test_mre, test_mae, test_rmse = model_run(test_x, test_y, 1.0, test_num)
             train_mre_reult.append(train_mre)
             test_mre_reult.append(test_mre)
-            if i % 1000 == 0:
-                current_time = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-                test_result = '\n%s\t%d\t%d\t\t%d\t%d\t%.1f\t%.4f\t%.2f\t%.2f' % (
-                    current_time, layer_num, hidden_size, timestep_size, i, dropout_rate, test_mre, test_mae, test_rmse)
-                with open('Result.txt', 'a') as fp:
-                    fp.write(test_result)
+            if i % 10 == 0:
+                current_time = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
                 end_time = datetime.datetime.now()
-                print(end_time - start_time)
+                time_spend = (end_time - start_time).seconds
+                start_time = datetime.datetime.now()
+                test_result = '\n%s\t%d\t%d\t\t%d\t%d\t%.1f\t%.4f\t%.2f\t%.2f\t%s' % (
+                    current_time, layer_num, hidden_size, time_step_size, i, dropout_keep_rate, test_mre, test_mae,
+                    test_rmse, time_spend)
+                with open(file_name, 'a') as fp:
+                    fp.write(test_result)
             print('epoch ', i, 'train', train_mre, train_mae, train_rmse, 'test', test_mre, test_mae, test_rmse)
-        sess.run(train_op, feed_dict={_X: train_x, _Y: train_y, keep_prob: dropout_rate, batch_size: train_num})
+        sess.run(train_op, feed_dict={_X: train_x, _Y: train_y, keep_prob: dropout_keep_rate, batch_size: train_num})
+    print('min_mre=%.4f\n' % np.min(test_mre_reult))
+
+    # 画图
     plt.plot(train_mre_reult, 'b-', label='train')
     plt.plot(test_mre_reult, 'r-', label='test')
     plt.legend()
-    plt.savefig('result_picture/hn%dts%ddp%.f.png' % (hidden_size, timestep_size, dropout_rate))
+    plt.savefig('result_picture/hn%dts%ddp%.f.png' % (hidden_size, time_step_size, dropout_keep_rate))
     plt.show()
     sess.close()
     tf.reset_default_graph()
     return train_mre_reult, test_mre_reult
-
-# test_mre, test_mae, test_rmse = model_run(train_x, train_y, 1.0, train_num)
-# current_time = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-# test_result = '\n%s\t%d\t%d\t\t%d\t%d\t%.4f\t%.2f\t%.2f' % (
-#     current_time, layer_num, hidden_size, timestep_size, max_epoch, test_mre, test_mae, test_rmse)
-# with open('./Result.txt', 'a') as fp:
-#     fp.write(test_result)
-
 
 # def draw_picture():
 #     re = tf.reduce_mean(tf.div(tf.abs(tf.subtract(_Y, y_pre)), _Y), 0)
