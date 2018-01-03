3632e7e315c1ff51c42468ca1fd0990bc34b5892
crown <crown-huang@foxmail.com>
2018-01-04 00:10:39 +0800

    add dbn in lstm_rbm_fso_f.py

--- lstm_rbm_fso_f.py
+++ lstm_rbm_fso_f.py
@@ -3,15 +3,17 @@ import tensorflow as tf
 from get_data import *
 from lstm_multicell import lstm_test
 from tfrbm import GBRBM
+from dbn_model import dbn
 
 gpu_device = 1
 time_step = 4
 train_num = 71 * 96
 test_num = 18 * 96
 # lstm的hyper-parameters
-# hidden_size = 400
+# dbn_hidden_size = 400
 data_type = 'normal'
-layer_num = 1
+lstm_layer_num = 1
+dbn_hidden_num = 3
 max_epoch = 20000
 dropout_keep_rate = 0.9
 
@@ -23,19 +25,18 @@ else:
     x = np.hstack((flow_normalized, speed_normalized, occupancy_normalized))
     y = flow_normalized
 train_x, train_y, test_x, test_y = create_train_test(x, y, time_step, train_num, test_num)
+dbn_x = train_x[:, 0, :]
 
-for rbm_hidden in [400]:
+for dbn_hidden_size in [400]:
     with tf.device('/gpu:%d' % gpu_device):
         # 训练rbm
-        _, _, input_size = train_x.shape
-        rbm = GBRBM(n_visible=input_size, n_hidden=rbm_hidden, learning_rate=0.01, momentum=0.95, use_tqdm=False)
-        errs = rbm.fit(train_x[:, 0, :], n_epoches=10, batch_size=100, verbose=True)
-        rbm_W, vb, rbm_b = rbm.get_weights()
+        _, _, dbn_input_size = train_x.shape
+        weights, biases = dbn(dbn_hidden_num, dbn_input_size, dbn_hidden_size, dbn_x)
 
     # 存储运行结果的文件
     # date_time = time.strftime('%Y-%m-%d', time.localtime(time.time()))
     file_name = 'fso_f_lstm_gbrbm'
     for hidden_size in [500]:
-        test_mre, test_mae, test_rmse = lstm_test(hidden_size, layer_num, max_epoch, dropout_keep_rate,
+        test_mre, test_mae, test_rmse = lstm_test(hidden_size, lstm_layer_num, max_epoch, dropout_keep_rate,
                                                   train_x, train_y, test_x, test_y, file_name,
-                                                  weights=rbm_W, biases=rbm_b, gpu_device=gpu_device)
+                                                  weights=weights, biases=biases, gpu_device=gpu_device)
