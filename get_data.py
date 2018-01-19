2ab492079b11a9cea52483cca5c4f920f7b8837e
crown <crown-huang@foxmail.com>
2018-01-19 19:27:40 +0800

    add get_aver_diff to get_data.py

--- get_data.py
+++ get_data.py
@@ -1,11 +1,10 @@
 import scipy.io as sio
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-def abnormal_data_process(data):
+def abnormal_data_process_for_occupancy(data):
     # 从0开始的24,25,26,27需要去掉
-    data = np.delete(data, [24, 25, 26, 27, 51, 95, 105], axis=1)
+    data = np.delete(data, [24, 25, 26, 27], axis=1)
     return data
 
 
@@ -32,6 +31,60 @@ def inverse_standardize(data_standard, data_mean, data_std):
     return inverse_data
 
 
+def abnormal_data_process_for_week(data):
+    data = np.delete(data, [51, 53, 89, 90, 93, 94, 95, 96, 97, 98, 103, 105], axis=1)
+    return data
+
+
+# def diff_to_original(flow_diff, week_aver):
+
+
+def get_aver_diff():
+    ld = sio.loadmat('data/new147k1.mat')
+    flow_data = abnormal_data_process_for_week(ld['data']) * 1956
+    flow_data[flow_data <= 0] = 3
+    station_num = flow_data.shape[1]
+    flow_diff = np.zeros(shape=[90 * 96, station_num])
+    week_aver = np.zeros(shape=flow_diff.shape)
+
+    def data_diff(first_day, after_abnormal_day, weeks):
+        x = range((first_day - 1) * 96, first_day * 96)
+        mflow = flow_data[x, :][np.newaxis, :]
+        for i in weeks:
+            x = range(((first_day - 1) + i * 7) * 96, (first_day + i * 7) * 96)
+            f = flow_data[x, :][np.newaxis, :]
+            mflow = np.concatenate((mflow, f), axis=0)
+        x = range((after_abnormal_day - 1) * 96, after_abnormal_day * 96)
+        f = flow_data[x, :][np.newaxis, :]
+        mflow = np.concatenate((mflow, f), axis=0)
+        mflow_mean = mflow.mean(axis=0)
+
+        x = range((first_day - 1) * 96, first_day * 96)
+        flow_diff[x, :] = flow_data[x, :] - mflow_mean
+        week_aver[x, :] = mflow_mean
+        for i in weeks:
+            x = range((first_day - 1 + i * 7) * 96, (first_day + i * 7) * 96)
+            flow_diff[x, :] = flow_data[x, :] - mflow_mean
+            week_aver[x, :] = mflow_mean
+        for i in range(0, 3):
+            x = range((after_abnormal_day + i * 7 - 1) * 96, (after_abnormal_day + i * 7) * 96)
+            flow_diff[x, :] = flow_data[x, :] - mflow_mean
+            week_aver[x, :] = mflow_mean
+        return mflow_mean
+
+    week_aver = list()
+    week_aver.append(data_diff(7, 69, [1, 3, 4, 5, 7, 8, 9]))
+    week_aver.append(data_diff(8, 70, range(1, 10)))
+    week_aver.append(data_diff(2, 71, range(1, 11)))
+    week_aver.append(data_diff(3, 72, range(1, 11)))
+    week_aver.append(data_diff(4, 73, range(1, 11)))
+    week_aver.append(data_diff(5, 74, range(1, 11)))
+    week_aver.append(data_diff(6, 75, range(1, 10)))
+    x = range(89 * 96, 90 * 96)
+    flow_diff[x, :] = flow_data[x, :] - week_aver[0]  # 为了产生train和test数据方便，最后加了一天
+    return flow_diff, week_aver
+
+
 def create_train_test(x, y, step, train_num, test_num):
     train_x = []
     train_y = []
@@ -55,80 +108,26 @@ def create_train_test(x, y, step, train_num, test_num):
 
 
 _load_data = sio.loadmat('data/new147k1.mat')
-_flow = abnormal_data_process(_load_data['data']) * 1956
+_flow = abnormal_data_process_for_occupancy(_load_data['data']) * 1956
 _flow[_flow <= 0] = 3
 
 _pems = np.load('data/pems_speed_occupancy_15min.npz')
-_speed = abnormal_data_process(_pems['speed'])
-_occupancy = abnormal_data_process(_pems['occupancy'])
+_speed = abnormal_data_process_for_occupancy(_pems['speed'])
+_occupancy = abnormal_data_process_for_occupancy(_pems['occupancy'])
 _occupancy[_occupancy <= 0] = 0.0002
 
 flow_normalized, flow_min, flow_max = normalize(_flow)
 speed_normalized, speed_min, speed_max = normalize(_speed)
 occupancy_normalized, occupancy_min, occupancy_max = normalize(_occupancy)
 
-flow_standardized, flow_mean, flow_std = standardize(_flow)
-speed_standardized, speed_mean, speed_std = standardize(_speed)
-occupancy_standardized, occupancy_mean, occupancy_std = standardize(_occupancy)
-
-# def monday_aver_flow(train_num):
-flow_diff = _flow.copy()[0:89 * 96, :]
-
-# x = range(6 * 96, 7 * 96)
-# mflow = _flow[x, :][np.newaxis, :]
-# # plt.plot(_flow[6 * 96:7 * 96, station])
-# # plt.show()
-# for i in [1, 3, 4, 5, 7, 8, 9]:
-#     x = range((6 + i * 7) * 96, (7 + i * 7) * 96)
-#     # plt.plot(_flow[x, station])
-#     # plt.show()
-#     f = _flow[x, :][np.newaxis, :]
-#     mflow = np.concatenate((mflow, f), axis=0)
-# x = range(68 * 96, 69 * 96)
-# f = _flow[x, :][np.newaxis, :]
-# mflow = np.concatenate((mflow, f), axis=0)
-# mflow_mean = mflow.mean(axis=0)
-#
-# x = range(6 * 96, 7 * 96)
-# flow_diff[x, :] = flow_diff[x, :] - mflow_mean
-# for i in range(1, 10):
-#     x = range((6 + i * 7) * 96, (7 + i * 7) * 96)
-#     flow_diff[x, :] = flow_diff[x, :] - mflow_mean
-# for i in range(0, 3):
-#     x = range((68 + i * 7) * 96, (69 + i * 7) * 96)
-#     flow_diff[x, :] = flow_diff[x, :] - mflow_mean
+# flow_standardized, flow_mean, flow_std = standardize(_flow)
+# speed_standardized, speed_mean, speed_std = standardize(_speed)
+# occupancy_standardized, occupancy_mean, occupancy_std = standardize(_occupancy)
 
 
-def data_diff(first_day, abnormal_day, weeks):
-    x = range((first_day - 1) * 96, first_day * 96)
-    mflow = _flow[x, :][np.newaxis, :]
-    for i in weeks:
-        x = range(((first_day - 1) + i * 7) * 96, (first_day + i * 7) * 96)
-        f = _flow[x, :][np.newaxis, :]
-        tflow = np.concatenate((mflow, f), axis=0)
-    x = range((abnormal_day - 1) * 96, abnormal_day * 96)
-    f = _flow[x, :][np.newaxis, :]
-    mflow = np.concatenate((mflow, f), axis=0)
-    mflow_mean = mflow.mean(axis=0)
-
-    x = range((first_day - 1) * 96, first_day * 96)
-    flow_diff[x, :] = flow_diff[x, :] - mflow_mean
-    for i in weeks:
-        x = range((6 + i * 7) * 96, (7 + i * 7) * 96)
-        flow_diff[x, :] = flow_diff[x, :] - mflow_mean
-    for i in range(0, 3):
-        x = range((abnormal_day + i * 7 - 1) * 96, (abnormal_day + i * 7) * 96)
-        flow_diff[x, :] = flow_diff[x, :] - mflow_mean
-    return mflow_mean
-
-
-station = 0
-first_day = 7
-abnormal_day = 69
-weeks = [1, 3, 4, 5, 7, 8, 9]
-
-ax = data_diff(first_day, abnormal_day, weeks)
-
+# a = np.argwhere(np.abs(flow_diff) > 300)
+# dict(zip(*np.unique(a[:, 1], return_counts=True)))
+# print(np.unique(a[:, 1], return_counts=True))
 # plt.plot(flow_diff[x, :])
 # plt.show()
 
