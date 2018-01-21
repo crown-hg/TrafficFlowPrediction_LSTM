91d37a34d2ea9690829ac2e9b06466ae65c1db30
crown <crown-huang@foxmail.com>
2018-01-21 22:57:50 +0800

    trend test for f_f lstm_rbm

--- lstm_rbm_f_f_trend.py
+++ lstm_rbm_f_f_trend.py
@@ -4,7 +4,7 @@ import trend_data_diff_aver as tdda
 from lstm_multicell import lstm_test
 from dbn_model import dbn
 
-gpu_device = 1
+gpu_device = 2
 time_step = 3
 train_num = 75 * 96
 test_num = 14 * 96
@@ -18,14 +18,13 @@ dropout_keep_rate = 0.9
 hidden_size = 200
 
 # 取数据
-train_x, train_y, train_y_aver, test_x, test_y, test_y_aver, flow_diff_min, flow_diff_max = \
-    tdda.create_train_test_week(time_step, train_num, test_num)
+data = tdda.create_train_test_week(time_step, train_num, test_num)
 
-dbn_x = train_x[:, 0, :]
-for dbn_hidden_size in [300]:
+dbn_x = data['train_x'][:, 0, :]
+for dbn_hidden_size in [100]:
     with tf.device('/gpu:%d' % gpu_device):
         # 训练rbm
-        _, _, dbn_input_size = train_x.shape
+        _, _, dbn_input_size = data['train_x'].shape
         weights, biases = dbn(dbn_hidden_num, dbn_input_size, dbn_hidden_size, dbn_x)
     for hidden_size in [100, 200, 300, 400]:
         file_name = 'f_f_trend_lstm_rbm'
